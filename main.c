c88679d43d5d758b093212cdabc5afe7ee3ce87b
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-11-30 16:51:34 +0100

    use curl to get twitter search results

--- main.c
+++ main.c
@@ -2,16 +2,39 @@
 #include <string.h>
 
 #include <jansson.h>
+#include <curl/curl.h>
+
 #include "entities.h"
+
+static size_t callback_func(void *ptr, size_t size, size_t count, void *stream)
+{
+    char **json = (char **) stream;
+
+    *json = malloc(size * count + 1);
+    memcpy(*json, ptr, size * count);
+    (*json)[size * count] = '\0';
+    return size * count;
+}
 
 int main(int argc, char *argv[])
 {
-    const char *json = "{\"test\":\"&nbsp;\\u2619\\u2661\\u2665\\u2763\\u2764\\u2765\\u2766\\u2767&amp;&lt;&quot;Jürgen\"}";
+    char *json = NULL;
     json_t *root;
     json_error_t error;
     json_t *commits;
     const char *message_text;
     char *result;
+    CURL *curl;
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://search.twitter.com/search.json?q=to:fgraum&rpp=3");
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &json);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback_func);
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        printf("%s\n", json);
+    }
 
     root = json_loads(json, 0, &error);
     commits = json_object_get(root, "test");
